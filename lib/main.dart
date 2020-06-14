dac7bbc91c81cc0ade29a66d234b7d0d1e49854d
OchiaiShun <minnanouta.bass@gmail.com>
2020-06-14 14:34:49 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter DemoDemoDemo',
       home: ChangeNotifierProvider<MainModel>(
         create: (_) => MainModel(),
         child: Scaffold(
